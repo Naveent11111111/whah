c641869deed1ac29a27e3d67c29aeda06c915505
Naveen <naveentgoud1@gmail.com>
2025-05-24 14:43:37 +0530

    in

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import boto3
 ec2 = boto3.resource('ec2')
 
 instance = ec2.create_instances(
-    ImageId='ami-0c02fb55956c7d316',  # Amazon Linux 2 (us-east-1 region)
+    ImageId='956c7d316',  # Amazon Linux 2 (us-east-1 region)
     InstanceType='t2.micro',
     MinCount=1,
     MaxCount=1
