7bc3d54194a5199ffcd72b591f739ff15f189e79
root <root@ip-172-31-15-208.ap-south-1.compute.internal>
2025-05-24 09:17:57 +0000

    thop

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
     ImageId='956c7d316',  # Amazon Linux 2 (us-east-1 region)
     InstanceType='t2.micro',
-    MinCount=1,
+    MinCunt=1,
     MaxCount=1
 )
